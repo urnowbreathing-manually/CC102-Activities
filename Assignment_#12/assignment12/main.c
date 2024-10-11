3a4b13c45cb61b9a005d83f4ab2d84574b9abce0
urnowbreathing-manually <andreizantua.ucc1@gmail.com>
2024-10-11 18:32:50 +0800

    Finsihed Assignment #12 - Continue?

--- Assignment_#12/assignment12/main.c
+++ Assignment_#12/assignment12/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+
+int a = 89;
 
 int main(){
+    while(a == 89 || a == 121){
+        printf("Do you want to continue? [Y/y]: ");
+        do{
+            a = getchar();
+        } while (isspace(a));
 
+    }
 
-
+    return 0;
 }
+
