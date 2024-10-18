a5410641697b7f1c8af2262d0885d1a414dc18cb
urnowbreathing-manually <andreizantua.ucc1@gmail.com>
2024-10-19 06:01:53 +0800

    Initial Commit of Assignment #14 - ASCII Shape Visualizer || Implemented program navigation function

---
+++ Assignment_#14/assignment14/main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int shapeSelector = 16;
+char shapeNouns[17][18] = {"Exit", "Line", "Striped Line", "Square", "Parallelogram", "Triangle", "Reverse Triangle",
+                           "Isoceles Triangle", "Reverse Isoceles", "Hour Glass", "Diamond", "Zero",
+                           "Up Arrow", "Down Arrow", "Letter X", "Bow Tie", "Invalid Input"};
+
+int shapeSelection();
+
+int main(){
+    while(shapeSelector != 0){
+        shapeSelection();
+        switch(shapeSelector){
+            case 1:
+
+            case 2:
+
+            case 3:
+
+            case 4:
+
+            case 5:
+
+            case 6:
+
+            case 7:
+
+            case 8:
+
+            case 9:
+
+            case 10:
+
+            case 11:
+
+            case 12:
+
+            case 13:
+
+            case 14:
+
+            case 15:
+
+            default:
+                printf("\n");
+
+        }
+
+    }
+
+    return 0;
+}
+
+int shapeSelection(){
+    //UI text for readability
+    printf("O============(ASCII Shape  Visualizer)============O\n");
+    printf("Selection:\n");
+    printf("[0 =       %s       ] [ 8 = %s]\n", shapeNouns[0], shapeNouns[8]);
+    printf("[1 =       %s       ] [ 9 =    %s   ]\n", shapeNouns[1], shapeNouns[9]);
+    printf("[2 =   %s   ] [10 =     %s     ]\n", shapeNouns[2], shapeNouns[10]);
+    printf("[3 =      %s      ] [11 =       %s      ]\n", shapeNouns[3], shapeNouns[11]);
+    printf("[4 =   %s  ] [12 =     %s    ]\n", shapeNouns[4], shapeNouns[12]);
+    printf("[5 =     %s     ] [13 =    %s   ]\n", shapeNouns[5], shapeNouns[13]);
+    printf("[6 = %s ] [14 =     %s    ]\n", shapeNouns[6], shapeNouns[14]);
+    printf("[7 = %s] [15 =     %s     ]\n", shapeNouns[7], shapeNouns[15]);
+    printf("\nSelect Shape to Visualize: ");
+
+    //Assigns value to shapeSelector for program naviagation
+    scanf("%d", &shapeSelector);
+    printf("Selected Visualization: [#%d] - [%s]\n\n", shapeSelector, shapeNouns[shapeSelector]);
+
+    return 0;
+}
